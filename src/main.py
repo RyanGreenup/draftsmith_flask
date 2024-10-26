2b30954e4a16e009fa8683b1411d497e03c76df2
ryan <exogenesis@protonmail.com>
2024-10-26 22:22:00 +1100

    begin fleshing out cli

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import typer
 from server import app  # Import the Flask app
 
 
-def main(port: int = 8080, host: str = "127.0.0.1"):
+def main(port: int = 8080, host: str = "0.0.0.0", debug: bool = False, api_host: str = "localhost", api_port: int = 37238):
     if host == "0.0.0.0":
         print("Warning: Server is accessible from any IP address. Use with caution.")
     print(f"Serving on {host}:{port}")
